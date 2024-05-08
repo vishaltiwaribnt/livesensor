58cd4d4f0332e523469873398c0d6649ef252dd8
Vishal Tiwari <vishaltiwaribnt@gmail.com>
2024-05-08 10:39:45 +0530

    edit privious setup on commandsfro l to L

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages,setup
 #from typing import List
 
 
-def get_requirements()->list[str]:
+def get_requirements()->List[str]:
 
-    reuirements_list = list[str] = []
+    reuirements_list : List[str] = []
 
     return reuirements_list
 
 
-
+ 
 setup(
     name ='sensor',
     version = "0.0.1",
